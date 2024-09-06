2b40131b3216da6c91d3d5d55646bbc5cf9b900b
Medet Ahmetson <milayter@gmail.com>
2024-09-07 01:31:34 +0700

    Remove site icon emoji

--- packages/app/src/app/layout.tsx
+++ packages/app/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 import { PropsWithChildren } from 'react'
-import { SITE_DESCRIPTION, SITE_EMOJI, SITE_INFO, SITE_NAME, SITE_URL, SOCIAL_TWITTER } from '@/utils/site'
+import { SITE_DESCRIPTION, SITE_INFO, SITE_NAME, SITE_URL, SOCIAL_TWITTER } from '@/utils/site'
 import { Layout } from '@/components/Layout'
 import { Web3Provider } from '@/context/Web3'
 import { NotificationProvider } from '@/context/Notifications'
