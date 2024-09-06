a2d1d503bdbbfece097cc40f765b979121d8703f
Medet Ahmetson <milayter@gmail.com>
2024-09-06 23:47:52 +0700

    Clean the unwrap component

--- packages/app/src/components/Unwrap.tsx
+++ packages/app/src/components/Unwrap.tsx
@@ -1,24 +1,12 @@
 'use client'
-import {
-  useAccount,
-  useAccountEffect,
-  useReadContract,
-  useSimulateContract,
-  useWaitForTransactionReceipt,
-  useWriteContract,
-} from 'wagmi'
+import { useAccount, useAccountEffect } from 'wagmi'
 import { useEffect, useState } from 'react'
-import { useNotifications } from '@/context/Notifications'
 import { GetAddr } from '@/utils/web3'
-import { RedeemCard } from './RedeemAlert'
-import { NftType } from '@/utils/types'
 
 export const Unwrap = () => {
-  const { Add } = useNotifications()
   const account = useAccount()
   const [connected, setConnected] = useState<boolean>(false)
-  const [nfts, setNfts] = useState<NftType[]>()
-  const [lpNftAddress, setLpNftAddress] = useState<`0x${string}`>()
+  const [lpManagerAddress, setLpManagerAddress] = useState<`0x${string}`>()
   useAccountEffect({
     onConnect() {
       setConnected(true)
@@ -30,25 +18,7 @@ export const Unwrap = () => {
 
   useEffect(() => {
     if (connected) {
-      setLpNftAddress(GetAddr('lpNftAddress', account.chainId!))
-
-      const url = process.env.NEXT_PUBLIC_BACKEND_URL!
-
-      fetch(`${url}/nfts/${account!.chainId}/${account!.address}`).then((response) => {
-        response
-          .json()
-          .then((data) => {
-            if (data['message'] !== undefined) {
-              Add('Server error: ' + data['message'], { type: 'error' })
-            } else {
-              setNfts(data as NftType[])
-            }
-          })
-          .catch((error) => {
-            Add('Failed to parse incoming data from server', { type: 'error' })
-            console.error(error)
-          })
-      })
+      setLpManagerAddress(GetAddr('lpManagerAddress', account.chainId!))
     }
   }, [connected])
 
